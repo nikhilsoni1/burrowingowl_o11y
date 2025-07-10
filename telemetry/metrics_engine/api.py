afb4a9d10c9cf470a7ef83804b458e262945e7a7
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2025-07-10 11:48:40 -0700

    Add TailscaleStatus metric and update logging in get_all_metric_values (#4)
    Co-authored-by: Nikhil Soni <soni.nikhil@outlook.com>

--- telemetry/metrics_engine/api.py
+++ telemetry/metrics_engine/api.py
@@ -8,7 +8,7 @@ def get_all_metric_values():
     for name, instance in metric_registry.all().items():
         try:
             results[name] = instance.generate_metric()
-            logger.info(f"Generated metric '{name}': {results[name]}")
+            logger.info(f"Generated metric '{name}'")
         except Exception as e:
             results[name] = None
             logger.error(f"Error generating metric '{name}': {e}")
