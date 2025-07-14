08ae4128475bdf09204cf41d1b2a6938cde9c6b5
Nikhil Soni <soni.nikhil@outlook.com>
2025-07-14 11:29:26 +0530

    Refactor Tailscale status processing to separate peer and user data into dedicated functions

--- telemetry/metrics_engine/metrics/tailscale_status.py
+++ telemetry/metrics_engine/metrics/tailscale_status.py
@@ -2,6 +2,23 @@ import subprocess
 import json
 from telemetry.metrics_engine.base import Metric
 
+def peer_to_list(peer):
+    peer_list = list()
+    for k, v in peer.items():
+        peer_record = dict()
+        peer_record["node_key"] = k
+        peer_record["node_data"] = v
+        peer_list.append(peer_record)
+    return peer_list
+
+def user_to_list(user):
+    user_list = list()
+    for k, v in user.items():
+        user_record = dict()
+        user_record["user_key"] = k
+        user_record["user_data"] = v
+        user_list.append(user_record)
+    return user_list
 
 class TailscaleStatus(Metric):
     metric_name = "tailscale_status"
@@ -16,7 +33,12 @@ class TailscaleStatus(Metric):
                 text=True,
             )
             status_data = json.loads(result.stdout)
-            # status_data.pop("Peer", None)
+            peer = status_data.pop("Peer", None)
+            peer_list = peer_to_list(peer) if peer else list()
+            status_data["Peer"] = peer_list
+            user = status_data.pop("User", None)
+            user_list = user_to_list(user) if user else list()
+            status_data["User"] = user_list
             return status_data
         except (
             subprocess.CalledProcessError,
